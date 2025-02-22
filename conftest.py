4249bfb3aaf9c4cec869e44dfa8a14cf037c52fd
danilawower <92742319+danilawower@users.noreply.github.com>
2025-02-22 14:42:41 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -11,10 +11,10 @@ from selenium.webdriver.chrome.options import Options
 
 @pytest.fixture(scope='session')
 def driver():
-    chrome_options = Options()
+    chrome_options = webdriver.ChromeOptions()
     chrome_options.add_argument('--incognito')
     chrome_options.add_argument("--disable-gpu")
-    driver = webdriver.Chrome()
+    driver = webdriver.Chrome(options=chrome_options)
     driver.maximize_window()
     attach = driver.get_screenshot_as_png()
     allure.attach(attach, name=f'Screenshot {datetime.today()}', attachment_type=allure.attachment_type.PNG)
