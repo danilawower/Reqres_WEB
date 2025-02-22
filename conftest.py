fd11ba1e295a91c12b30bf2d29b9795c5311ed4d
danilawower <92742319+danilawower@users.noreply.github.com>
2025-02-22 14:44:10 +0300

    Update conftest.py

--- conftest.py
+++ conftest.py
@@ -12,10 +12,12 @@ from selenium.webdriver.chrome.options import Options
 @pytest.fixture(scope='session')
 def driver():
     chrome_options = webdriver.ChromeOptions()
-    chrome_options.add_argument('--incognito')
-    chrome_options.add_argument("--disable-gpu")
+    chrome_options.add_argument("--headless")
+    chrome_options.add_argument("--incognito")
+    chrome_options.add_argument("--ignore-certificate-errors")
+    chrome_options.add_argument("--window-size=1920,1080")
+    chrome_options.add_argument("--disable-cache")
     driver = webdriver.Chrome(options=chrome_options)
-    driver.maximize_window()
     attach = driver.get_screenshot_as_png()
     allure.attach(attach, name=f'Screenshot {datetime.today()}', attachment_type=allure.attachment_type.PNG)
     yield driver
